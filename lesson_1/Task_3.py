d855e2f7021ccb7e1d020b3e510089c64466feab
gkozhemiakin <kga.2004@mail.ru>
2023-03-20 08:26:49 +0300

    Сделал вывод в строчку

--- lesson_1/Task_3.py
+++ lesson_1/Task_3.py
@@ -1,3 +1,3 @@
-array = list(map(float, input().split()))
-for i in range(len(array)):
-    print(abs(int(array[i])), end = ' ')
+Array = list(map(float, input().split()))
+for i in range(len(Array)):
+    print(abs(int(Array[i])), end=' ')
